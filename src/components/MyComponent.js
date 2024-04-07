36ac4d7e01c51c0f0391dce755dcc1951306092b
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2024-04-07 10:58:29 +0700

    Video 17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,11 +4,18 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Cucumber',
+        address: 'NguyenLeDuy1012',
+        age: 21,
+    };
+
     render() {
         return (
             <div>
-                My first component
-                {Math.random()}
+
+                My name is {this.state.name} and I'm from {this.state.address}
             </div>
         )
     }
