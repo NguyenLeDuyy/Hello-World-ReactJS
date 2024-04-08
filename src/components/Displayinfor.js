a34c23b5a2055c4fe6ef630739844e27da3324a0
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2024-04-08 21:03:02 +0700

    Video 33

--- src/components/Displayinfor.js
+++ src/components/Displayinfor.js
@@ -4,9 +4,29 @@ import logooo from './../logo.svg'; //logooo tượng trưng cho tên biến
 
 class DisplayInfor extends React.Component {
 
+    constructor(props) {
+        console.log('>>>> call me constructor: 0')
+        super(props);
+        //babel compiler
+        this.state = {
+            isShowListUser: true,
+        }
+    }
 
-    state = {
-        isShowListUser: true,
+    componentDidMount() {
+        console.log('call me component did mount');
+        setTimeout(() => {
+            document.title = 'Duy Lê'
+        }, 3000);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('call me component did update', this.props, prevProps);
+        if (this.props.listUsers !== prevProps.listUsers) {
+            if (this.props.listUsers.length === 5) {
+                alert('You got 5 users')
+            }
+        }
     }
 
     handleShowHide = () => {
@@ -16,6 +36,7 @@ class DisplayInfor extends React.Component {
     }
 
     render() {
+        console.log('>>>> call me render')
 
         //destructuring array/object
         const { listUsers } = this.props; // "Vì" this.props là một object variable
@@ -39,6 +60,15 @@ class DisplayInfor extends React.Component {
                                     <div>My age's {user.age}</div>
                                     <div>
                                         <button onClick={() => { this.props.handleDeleteUser(user.id) }}>Delete</button>
+                                        {/* Trong React, khi bạn truyền một hàm như một prop và muốn truyền tham số cho hàm đó, bạn cần sử dụng một arrow function hoặc một hàm bình thường để "bọc" lời gọi hàm.
+
+                                        Nếu bạn viết như sau:
+
+                                        Thì hàm handleDeleteUser sẽ được gọi ngay lập tức khi component được render, chứ không phải khi bạn click vào button. Điều này thường không phải là điều bạn mong muốn.
+
+                                        Khi bạn sử dụng một arrow function như sau:
+
+                                        Thì hàm handleDeleteUser sẽ chỉ được gọi khi bạn click vào button, và user.id sẽ được truyền vào như một tham số. Đây chính là cách bạn truyền tham số cho một hàm callback trong React. */}
                                     </div>
                                     <hr />
                                 </div>
