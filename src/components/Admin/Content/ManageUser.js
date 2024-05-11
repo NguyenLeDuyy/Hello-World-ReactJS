4adae7a6bb34a84e2c4f5c674c71b14f70913334
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2024-05-11 23:05:07 +0700

    video 65

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -5,12 +5,14 @@ import TableUser from "./TableUser";
 import { useEffect, useState } from "react";
 import { getAllUsers } from "../../../service/apiServices";
 import ModalUpdateUser from "./ModalUpdateUser";
+import ModalViewUser from "./ModalViewUser";
 
 
 const ManageUser = (props) => {
 
     const [showModalCreateUser, setShowModalCreateUser] = useState(false);
     const [showModalUpdateUser, setShowModalUpdateUser] = useState(false);
+    const [showModalViewUser, setShowModalViewUser] = useState(false);
     const [dataUpdate, setDataUpdate] = useState({});
 
 
@@ -29,9 +31,14 @@ const ManageUser = (props) => {
     }
 
     const handleClickBtnUpdate = (user) => {
-        setShowModalUpdateUser(true)
+        setShowModalUpdateUser(true);
         setDataUpdate(user);
-        console.log(user);
+        // console.log(user);
+    }
+
+    const handleClickBtnView = (user) => {
+        setShowModalViewUser(true);
+        setDataUpdate(user);
     }
 
     const resetUpdateData = () => {
@@ -54,6 +61,7 @@ const ManageUser = (props) => {
                     <TableUser
                         listUsers={listUsers}
                         handleClickBtnUpdate={handleClickBtnUpdate}
+                        handleClickBtnView={handleClickBtnView}
                     />
                 </div>
                 <ModalCreateUser
@@ -66,6 +74,13 @@ const ManageUser = (props) => {
                     setShow={setShowModalUpdateUser}
                     dataUpdate={dataUpdate}
                     fetchListUsers={fetchListUsers}
+                    resetUpdateData={resetUpdateData}
+                />
+                <ModalViewUser
+                    show={showModalViewUser}
+                    setShow={setShowModalViewUser}
+                    dataUpdate={dataUpdate}
+                    // fetchListUsers={fetchListUsers}
                     resetUpdateData={resetUpdateData}
                 />
             </div>
