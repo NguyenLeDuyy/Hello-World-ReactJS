8d1ef3644c3274cd72318e0ed8be77fb430ad241
Nguyễn Lê Duy <147262594+NguyenLeDuyy@users.noreply.github.com>
2024-05-15 20:48:47 +0700

    video 83

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getQuesByQuizId } from "../../service/apiServices";
+import _ from "lodash";
 
 const DetailQuiz = () => {
     const params = useParams();
     const quizId = params.id;
-    console.log("check params: ", params)
-
-    const [arrQues, setarrQues] = useState([]);
 
     useEffect(() => {
         fetchQuestions();
@@ -15,10 +13,28 @@ const DetailQuiz = () => {
 
     const fetchQuestions = async () => {
         const res = await getQuesByQuizId(Number(quizId))
-        console.log("check resp: ", res)
-        setarrQues(res.DT)
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                // Group the elements of Array based on `color` property
+                .groupBy("id")
+                // `key` is group's name (color), `value` is the array of objects
+                .map((value, key) => {
+                    let answer = [];
+                    let quesDescription, image = null;
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            quesDescription = item.description;
+                            image = item.image;
+                        }
+                        answer.push(item.answers);
+                    })
+                    return { questionId: key, answer, quesDescription, image } // ghi kiểu này bằng answer: answer
+                })
+                .value()
+            console.log("check data: ", data)
+        }
     }
-
     return (
         <div>
             Detail Quiz
